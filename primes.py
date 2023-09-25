94d26408b431c1d4f359fa8862e01b002278a147
Parth Chugh <parth.chugh@kcl.ac.uk>
2023-09-25 12:09:05 +0100

    Final solution

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError
     list = []
+    next_prime = 2
+    for i in range(0,number_of_primes):
+        list.append(next_prime)
+        next_prime = next_prime + 1
+        while not isPrime(next_prime):
+            next_prime = next_prime+1
     return list
+
+def isPrime(num):
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
